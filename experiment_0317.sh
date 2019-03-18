4b2918d7674cf59de7baa7bd5d5ee86d33473d6c
pcc.pku@gmail.com <pcc.pku@gmail.com>
2019-03-17 20:32:57 -0700

    try first 2 parts.

--- experiment_0317.sh
+++ experiment_0317.sh
@@ -29,24 +29,24 @@ python run_pretraining.py \
   --num_warmup_steps=10 \
   --learning_rate=2e-5
 
-python run_squad.py \
-  --vocab_file=$BERT_BASE_DIR/vocab.txt \
-  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
-  --init_checkpoint=$BERT_BASE_DIR/pretrain_on_squad/model.ckpt-20 \
-  --do_train=True \
-  --train_file=$SQUAD_DIR/fake_train-v2.0.json \
-  --do_predict=True \
-  --predict_file=$SQUAD_DIR/fake_train-v2.0.json \
-  --train_batch_size=$BATCH_SIZE \
-  --learning_rate=3e-2 \
-  --num_train_epochs=2.0 \
-  --max_seq_length=$MAX_SEQ_LEN \
-  --doc_stride=128 \
-  --output_dir=$SQUAD_DIR\
-  --version_2_with_negative=True
+# python run_squad.py \
+#   --vocab_file=$BERT_BASE_DIR/vocab.txt \
+#   --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+#   --init_checkpoint=$BERT_BASE_DIR/pretrain_on_squad/model.ckpt-20 \
+#   --do_train=True \
+#   --train_file=$SQUAD_DIR/fake_train-v2.0.json \
+#   --do_predict=True \
+#   --predict_file=$SQUAD_DIR/fake_train-v2.0.json \
+#   --train_batch_size=$BATCH_SIZE \
+#   --learning_rate=3e-2 \
+#   --num_train_epochs=2.0 \
+#   --max_seq_length=$MAX_SEQ_LEN \
+#   --doc_stride=128 \
+#   --output_dir=$SQUAD_DIR\
+#   --version_2_with_negative=True
 
-python $SQUAD_DIR/evaluate-v2.0.py \
-  $SQUAD_DIR/fake_train-v2.0.json \
-  $SQUAD_DIR/predictions.json \
-  --na-prob-file $SQUAD_DIR/null_odds.json
+# python $SQUAD_DIR/evaluate-v2.0.py \
+#   $SQUAD_DIR/fake_train-v2.0.json \
+#   $SQUAD_DIR/predictions.json \
+#   --na-prob-file $SQUAD_DIR/null_odds.json
 
